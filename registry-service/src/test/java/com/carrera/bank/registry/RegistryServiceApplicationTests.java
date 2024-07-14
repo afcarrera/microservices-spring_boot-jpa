1357a8363479ec0ec9db59ea3e471dcd0d3c35b9
Alberto Carrera <acarrera_l@hotmail.com>
2024-07-14 02:15:35 -0500

    Add eureka server

---
+++ registry-service/src/test/java/com/carrera/bank/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrera.bank.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
